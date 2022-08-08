7c6e18478d2556138a8848270738de72065dbb62
Andrew Scribner <ca.scribner+1@gmail.com>
2022-08-08 16:11:52 -0400

    iterate on workflows

--- .github/workflows/get-charm-paths.sh
+++ .github/workflows/get-charm-paths.sh
@@ -23,7 +23,7 @@ fi
 
 # Convert output to JSON string format
 # { charm_paths: [...] }
-CHARM_PATHS_LIST=$(echo $CHARM_PATHS | jq -c --slurp --raw-input 'split("\n")[:-1]')
+CHARM_PATHS_LIST=$(echo "$CHARM_PATHS" | jq -c --slurp --raw-input 'split("\n")[:-1]')
 
 echo "Found CHARM_PATHS_LIST: $CHARM_PATHS_LIST"
 
